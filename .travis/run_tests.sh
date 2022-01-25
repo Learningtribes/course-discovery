2e2fccf97f42a435ba673c33dfe45aff3ad64da8
Barry Paneer <barrypaneer@BarrydeMacBook-Pro.local>
2022-01-25 11:04:41 +0800

    testing Django & cryptography

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -15,6 +15,12 @@ echo '{ "allow_root": true }' > /root/.bowerrc
 make requirements
 make requirements.js
 
+pip uninstall cryptography
+pip install cryptography==3.3.2
+pip uninstall Django
+pip install Django==1.11.29
+pip list
+
 # Ensure documentation can be compiled
 make docs
 
